0e663ebd118d2c62e39f2c22911ba047d09c5301
Nurlord <352933nurzat@gmail.com>
2025-06-11 14:10:48 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: [process.env.CLIENT_URL],
+    // origin: [process.env.CLIENT_URL],
     credentials: true,
     exposedHeaders: ['set-cookie'],
   });
